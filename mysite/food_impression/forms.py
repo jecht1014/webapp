7968fa26dd3ba9d21e2bb54ba7418a392ca2e083
jecht1014 <s153049@matsu.shimane-u.ac.jp>
2020-03-03 04:45:18 +0900

    Add: add.htmlにcompanyとkindを追加出来るようにプログラムを追加

--- mysite/food_impression/forms.py
+++ mysite/food_impression/forms.py
@@ -1,6 +1,16 @@
-from .models import Food
+from .models import Food, Company, Kind
 from django import forms
 class FoodFormAdd(forms.ModelForm):
     class Meta:
         model = Food
         fields = ['company', 'kind', 'name','taste','value','remarks']
+
+class CompanyFormAdd(forms.ModelForm):
+    class Meta:
+        model = Company
+        fields = ['name']
+
+class KindFormAdd(forms.ModelForm):
+    class Meta:
+        model = Kind
+        fields = ['kind']
--- mysite/food_impression/views.py
+++ mysite/food_impression/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from .models import *
-from .forms import FoodFormAdd
+from .forms import FoodFormAdd, CompanyFormAdd, KindFormAdd
 
 def index(request):
     latest_food_list = Food.objects.order_by('-pub_date')[:5]
@@ -12,10 +12,19 @@ def detail(request, food_id):
     return render(request, 'food_impression/detail.html', {'food': food})
 
 def add(request):
-    if (request.method == 'POST'):
+    if 'add_food' in request.POST:
         obj = Food()
-        info = FoodFormAdd(request.POST, instance = obj)
+        info = FoodFormAdd(request.POST, instance=obj)
         info.save()
         return redirect(to='/food_impression')
-    
-    return render(request, 'food_impression/add.html', {'form':FoodFormAdd()})
+    elif 'add_company' in request.POST:
+        obj = Company()
+        info = CompanyFormAdd(request.POST, instance=obj)
+        info.save()
+        return redirect(to='/food_impression/add/')
+    elif 'add_kind' in request.POST:
+        obj = Kind()
+        info = KindFormAdd(request.POST, instance=obj)
+        info.save()
+        return redirect(to='/food_impression/add/')
+    return render(request, 'food_impression/add.html', {'food_form':FoodFormAdd(), 'company_form':CompanyFormAdd(), 'kind_form':KindFormAdd()})
